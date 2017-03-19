8b501c989bbe617e8198c4b67ae588327a83d253
Bernardo Anderson <bernardodsanderson@gmail.com>
2017-03-19 00:13:42 -0500

    Map working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AppBar from 'material-ui/AppBar';
 // import injectTapEventPlugin from 'react-tap-event-plugin';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 import './index.css';
 
-class App extends Component {
+// http://165.234.255.87:8080/feed/vehicle/byRoutes/14
+// http://165.234.255.87:8080/fixedroute/
+
+class App extends React.Component {
   render() {
+    const position = [51.505, -0.09];
     return (
       <div className="App">
         <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
           <AppBar title="Matbus Rocks!" iconClassNameRight="muidocs-icon-navigation-expand-more" />
         </MuiThemeProvider>
+
+        <Map center={position} zoom={11}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
