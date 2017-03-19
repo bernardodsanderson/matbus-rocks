722832e04b4465d77627cd9da2f65168779949cb
Bernardo Anderson <bernardodsanderson@gmail.com>
2017-03-19 01:08:16 -0500

    Small update to map

--- src/App.js
+++ src/App.js
@@ -32,9 +32,6 @@ class App extends React.Component {
           </Marker>
         </Map>
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
