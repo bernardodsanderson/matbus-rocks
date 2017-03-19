091f433213ff3e5c4924056381998fb0df459413
Bernardo Anderson <bernardodsanderson@gmail.com>
2017-03-18 20:14:38 -0500

    Add material design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from 'material-ui/AppBar';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+          <AppBar title="Matbus Rocks!" iconClassNameRight="muidocs-icon-navigation-expand-more" />
+        </MuiThemeProvider>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
