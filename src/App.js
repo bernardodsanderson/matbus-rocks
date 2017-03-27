0bffec6a3cab2c7f70ecbd1177041efa895297a0
Bernardo Anderson <bernardodsanderson@gmail.com>
2017-03-26 21:35:24 -0500

    Another link

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import './logo.svg';
 // http://165.234.255.87:8080/fixedroute/
 // http://www.openrouteservice.org
 // http://map.project-osrm.org/?z=17&center=46.874476%2C-96.769863&loc=46.873879%2C-96.768329&loc=46.872800%2C-96.773994&hl=en&alt=0
+// https://github.com/Project-OSRM/osrm-backend/wiki/Server-API---v4,-old
 
 class App extends React.Component {
 
